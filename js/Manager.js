4d1964beddc7c5a3cac53bc1f3cb3c250071207a
Anna Vasylyshyn <fortuna.me@ukr.net>
2017-06-23 20:09:07 +0300

    added validation manager.js

--- js/Manager.js
+++ js/Manager.js
@@ -1,9 +1,11 @@
 var users = new Array();
 var ids = 0;
 
-function createPeople() {
-    var u = new User(ids++);    
-    
+var createPeople = function () {
+    var u = new User(ids++);
+    // alert('cp');
+
+
     u.name = $('#name').val();
     u.sex = $('#sex').val();
     u.date = $('#bdate').val();
@@ -11,10 +13,38 @@ function createPeople() {
     u.phone = $('#phone').val();
     u.mail = $('#mail').val();
 
-    //Порадьте хтось як це написати краще))
-    if(u.name && u.sex && u.date && u.address && u.phone && u.mail){ u.render();}
+
+    console.log(u);
+
+    if (u.name && u.sex && u.date && u.address && u.phone && u.mail) {
+        u.render();
+        // alert('i');
+    }
 
     // console.log(u);
 
     return u;
+};
+
+function validate(cd) {
+    var adr = $('#address').val().split(' ').join('+');
+
+    console.log(adr);
+
+    $.ajax({
+        url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + adr + "&key=AIzaSyDQUJdc7IrJ9VxZwBD0M1FB91m2kELn1PM",
+        // contentType: "text/html; charset=UTF-8",
+        success: function (result) {
+            // alert('+');
+
+            cd();
+
+            $('#form').trigger('reset');
+
+        },
+        error: function (err) {
+            // alert('-');
+            return false;
+        }
+    });
 }
